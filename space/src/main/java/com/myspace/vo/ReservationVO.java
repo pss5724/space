e99798fb09f958842350e8ef06f3ad75a5fe3413
samsandy <42550036+samsandy@users.noreply.github.com>
2021-07-14 18:16:55 +0900

    리뷰vo, 예약vo

--- space/src/main/java/com/myspace/vo/ReservationVO.java
+++ space/src/main/java/com/myspace/vo/ReservationVO.java
@@ -1,5 +1,152 @@
 package com.myspace.vo;
 
 public class ReservationVO {
-
+	int rsid, rid, headcount, convenience1_num, convenience2_num, convenience3_num, beverage1_num, beverage2_num, beverage3_num, amount;
+	String name, hp, email, branch_name, room_name, type, corp_name, event_name, pay_type, etc_request;
+	double checkin_time, checkout_time, used_hours;
+	
+	
+	public int getRsid() {
+		return rsid;
+	}
+	public void setRsid(int rsid) {
+		this.rsid = rsid;
+	}
+	public int getRid() {
+		return rid;
+	}
+	public void setRid(int rid) {
+		this.rid = rid;
+	}
+	public int getHeadcount() {
+		return headcount;
+	}
+	public void setHeadcount(int headcount) {
+		this.headcount = headcount;
+	}
+	public double getCheckin_time() {
+		return checkin_time;
+	}
+	public void setCheckin_time(double checkin_time) {
+		this.checkin_time = checkin_time;
+	}
+	public double getCheckout_time() {
+		return checkout_time;
+	}
+	public void setCheckout_time(double checkout_time) {
+		this.checkout_time = checkout_time;
+	}
+	public double getUsed_hours() {
+		return used_hours;
+	}
+	public void setUsed_hours(double used_hours) {
+		this.used_hours = used_hours;
+	}
+	public int getConvenience1_num() {
+		return convenience1_num;
+	}
+	public void setConvenience1_num(int convenience1_num) {
+		this.convenience1_num = convenience1_num;
+	}
+	public int getConvenience2_num() {
+		return convenience2_num;
+	}
+	public void setConvenience2_num(int convenience2_num) {
+		this.convenience2_num = convenience2_num;
+	}
+	public int getConvenience3_num() {
+		return convenience3_num;
+	}
+	public void setConvenience3_num(int convenience3_num) {
+		this.convenience3_num = convenience3_num;
+	}
+	public int getBeverage1_num() {
+		return beverage1_num;
+	}
+	public void setBeverage1_num(int beverage1_num) {
+		this.beverage1_num = beverage1_num;
+	}
+	public int getBeverage2_num() {
+		return beverage2_num;
+	}
+	public void setBeverage2_num(int beverage2_num) {
+		this.beverage2_num = beverage2_num;
+	}
+	public int getBeverage3_num() {
+		return beverage3_num;
+	}
+	public void setBeverage3_num(int beverage3_num) {
+		this.beverage3_num = beverage3_num;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHp() {
+		return hp;
+	}
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBranch_name() {
+		return branch_name;
+	}
+	public void setBranch_name(String branch_name) {
+		this.branch_name = branch_name;
+	}
+	public String getRoom_name() {
+		return room_name;
+	}
+	public void setRoom_name(String room_name) {
+		this.room_name = room_name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getCorp_name() {
+		return corp_name;
+	}
+	public void setCorp_name(String corp_name) {
+		this.corp_name = corp_name;
+	}
+	public String getEvent_name() {
+		return event_name;
+	}
+	public void setEvent_name(String event_name) {
+		this.event_name = event_name;
+	}
+	public String getPay_type() {
+		return pay_type;
+	}
+	public void setPay_type(String pay_type) {
+		this.pay_type = pay_type;
+	}
+	public String getEtc_request() {
+		return etc_request;
+	}
+	public void setEtc_request(String etc_request) {
+		this.etc_request = etc_request;
+	}
+	
+	
+	
+	
+	
 }
--- space/src/main/java/com/myspace/vo/ReviewVO.java
+++ space/src/main/java/com/myspace/vo/ReviewVO.java
@@ -1,5 +1,76 @@
 package com.myspace.vo;
 
 public class ReviewVO {
+	
+	String id, rdate, rid, room_name, rsid, rcontent;
+	int locationlv, facilitylv, pricelv, servicelv, totallv;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getRdate() {
+		return rdate;
+	}
+	public void setRdate(String rdate) {
+		this.rdate = rdate;
+	}
+	public String getRid() {
+		return rid;
+	}
+	public void setRid(String rid) {
+		this.rid = rid;
+	}
+	public String getRoom_name() {
+		return room_name;
+	}
+	public void setRoom_name(String room_name) {
+		this.room_name = room_name;
+	}
+	public String getRsid() {
+		return rsid;
+	}
+	public void setRsid(String rsid) {
+		this.rsid = rsid;
+	}
+	public String getRcontent() {
+		return rcontent;
+	}
+	public void setRcontent(String rcontent) {
+		this.rcontent = rcontent;
+	}
+	public int getLocationlv() {
+		return locationlv;
+	}
+	public void setLocationlv(int locationlv) {
+		this.locationlv = locationlv;
+	}
+	public int getFacilitylv() {
+		return facilitylv;
+	}
+	public void setFacilitylv(int facilitylv) {
+		this.facilitylv = facilitylv;
+	}
+	public int getPricelv() {
+		return pricelv;
+	}
+	public void setPricelv(int pricelv) {
+		this.pricelv = pricelv;
+	}
+	public int getServicelv() {
+		return servicelv;
+	}
+	public void setServicelv(int servicelv) {
+		this.servicelv = servicelv;
+	}
+	public int getTotallv() {
+		return totallv;
+	}
+	public void setTotallv(int totallv) {
+		this.totallv = totallv;
+	}
+	
+	
 
 }
